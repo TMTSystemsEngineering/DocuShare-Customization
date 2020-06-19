b1acc432bc300f83689de03d2c9133e70973dff2
CriteriaFirst <support@criteriafirst.com>
2020-06-19 11:44:33 -0500

    Better admin control of DCN
    Allow admin to change some, and better report problems without losing Summary

--- Phase 1/database/tmt_dsobject_insert.sql
+++ Phase 1/database/tmt_dsobject_insert.sql
@@ -1,3 +1,13 @@
+-- FUNCTION: public.tmt_dsobject_insert()
+DROP TRIGGER tmt_dsobject_insert ON public.dsobject_table;
+DROP FUNCTION public.tmt_dsobject_insert();
+
+CREATE FUNCTION public.tmt_dsobject_insert()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF
+AS $BODY$
 declare
     parts varchar(32)[];
     leader varchar(80);
@@ -10,18 +20,33 @@ if NEW.handle_class = 2 then
         trailer := '\.[A-Z0-9]{4,5}';
 
         if exists (select 1 from tmt_dcn_issued as T where (T.dcn = leader or T.dcn similar to (leader || trailer) ) and T.date_uploaded is null) then
-	    update tmt_dcn_issued set date_uploaded = localtimestamp, ds_doc_handle='Document-' || NEW.handle_index where dcn = leader or dcn similar to (leader || trailer) ;
+            update tmt_dcn_issued 
+               set date_uploaded = localtimestamp
+                 , ds_doc_handle='Document-' || NEW.handle_index 
+             where dcn = leader or dcn similar to (leader || trailer) ;
         elseif exists (select 1 from tmt_dcn_issued as T where (T.dcn = leader or T.dcn similar to (leader || trailer) ) and T.date_uploaded is not null) then
-	    NEW.object_summary := 'This document''s DCN ' || NEW.document_dcn || ' duplicates an existing document''s';
-	    NEW.document_dcn := 'DUPLICATE';
+            NEW.object_summary := 'This document''s DCN ' || NEW.document_dcn || ' duplicates an existing document''s. ' || OLD.object_summary;
+            NEW.document_dcn := 'DUPLICATE';
         else
-	    NEW.object_summary := 'This document''s DCN ' || NEW.document_dcn || ' is invalid';
+            -- Cannot upload a document to a DCN that was never reserved
+            NEW.object_summary := 'This document''s DCN ' || NEW.document_dcn || ' has not been issued. ' || OLD.object_summary;
             NEW.document_dcn := 'INVALID';
         end if;
     else
-        NEW.object_summary := 'This document''s DCN ' || NEW.document_dcn || ' is invalid';
+        NEW.object_summary := 'This document''s DCN ' || NEW.document_dcn || ' is structurally invalid. ' || OLD.object_summary;
         NEW.document_dcn := 'INVALID';
     end if;
 end if;
 return NEW;
 end;
+$BODY$;
+
+ALTER FUNCTION public.tmt_dsobject_insert()
+    OWNER TO docushare_admin;
+
+CREATE TRIGGER tmt_dsobject_insert
+    BEFORE INSERT
+    ON public.dsobject_table
+    FOR EACH ROW
+    WHEN ((new.handle_class = (2)::numeric))
+    EXECUTE PROCEDURE public.tmt_dsobject_insert();
--- Phase 1/database/tmt_dsobject_update.sql
+++ Phase 1/database/tmt_dsobject_update.sql
@@ -1,29 +1,78 @@
+-- FUNCTION: public.tmt_dsobject_update()
+DROP TRIGGER tmt_dsobject_update ON public.dsobject_table;
+DROP FUNCTION public.tmt_dsobject_update();
+
 CREATE FUNCTION public.tmt_dsobject_update()
     RETURNS trigger
     LANGUAGE 'plpgsql'
     COST 100
     VOLATILE NOT LEAKPROOF
 AS $BODY$
+declare
+    oldParts varchar(32)[];
+	newParts varchar(32)[];
+	yearCalc integer;
 begin
-if OLD.handle_class = 2 then
-    if OLD.object_isdeleted = 0 and NEW.object_isdeleted = 1 then
-        -- Sent to trash
-        update tmt_dcn_issued set date_uploaded = NULL, ds_doc_handle = NULL where dcn = OLD.document_dcn;
-    elseif OLD.object_isdeleted = 1 and NEW.object_isdeleted = 0 then
-        -- Retrieved from trash
-        update tmt_dcn_issued set date_uploaded = OLD.object_create_date, ds_doc_handle = 'Document-' || OLD.handle_index where dcn = OLD.document_dcn;
-    end if;
-
-    if OLD.document_dcn != NEW.document_dcn then
-        -- unlink old DCN from this document
-		update tmt_dcn_issued set date_uploaded = NULL, ds_doc_handle = NULL where dcn = OLD.document_dcn;
-		-- link new DCN to this document
-		update tmt_dcn_issued set date_uploaded = NEW.object_create_date, ds_doc_handle = 'Document-' || NEW.handle_index where dcn = NEW.document_dcn;
+	-- if not a document, then just return NEW (i.e. "proceed")
+	if OLD.handle_class != 2 then 
+		return NEW;
 	end if;
-end if;
-return NEW;
+
+	-- if the DCN is unchanged, then just return NEW (i.e. "proceed")
+	if OLD.document_dcn = NEW.document_dcn then 
+		return NEW;
+	end if;
+
+	-- user changed the DCN. See if the new DCN is already in use
+	if exists (select 1 from tmt_dcn_issued as T where T.dcn = NEW.document_dcn and T.ds_doc_handle != ('Document-' || cast(NEW.handle_index as varchar))) then
+		NEW.object_summary := 'Attempt to change document''s DCN to a value already in use: ' || NEW.document_dcn || '. '  || COALESCE(OLD.object_summary, '');
+		-- undo the user's change to the DCN
+		NEW.document_dcn := OLD.document_dcn;
+	end if;
+
+	-- Break the DCN into constituent parts
+	oldParts := string_to_array(OLD.document_dcn, '.');
+	newParts := string_to_array(NEW.document_dcn, '.');
+
+	-- update the existing document's record
+	if exists (select 1 from tmt_dcn_issued as T where T.dcn = OLD.document_dcn) then
+		if (array_length(newParts, 1) = 6) then
+			yearCalc = COALESCE (cast (newParts[4] as integer), 1900);
+			update tmt_dcn_issued
+				set title                   = COALESCE (NEW.object_title, '')
+				  , keywords                = COALESCE (NEW.object_keywords, '')
+				  , telescope               = COALESCE (newParts[1], '')
+				  , group_category          = COALESCE (newParts[2], '')
+				  , document_type           = COALESCE (newParts[3], '')
+				  , year                    = CASE WHEN yearCalc < 80 THEN yearCalc + 2000 WHEN yearCalc < 100 THEN yearCalc + 1900 ELSE yearCalc END
+				  , sequence_number         = COALESCE (cast (newParts[5] as integer), 0)
+				  , document_version        = COALESCE (substring (newParts[6] from 1 for 3), '')
+				  , version_sequence_number = COALESCE (cast (substring (newparts[6] from 4 for 2) as integer), 0)
+				  , dcn                     = NEW.document_dcn
+				  , ds_doc_handle           = 'Document-' || NEW.handle_index
+				  , date_uploaded           = CURRENT_TIMESTAMP
+				where dcn = OLD.document_dcn;
+		else
+			update tmt_dcn_issued
+				set title                   = COALESCE (NEW.object_title, '')
+				  , keywords                = COALESCE (NEW.object_keywords, '')
+				  , dcn                     = NEW.document_dcn
+				  , ds_doc_handle           = 'Document-' || NEW.handle_index
+				  , date_uploaded           = CURRENT_TIMESTAMP
+				where dcn = OLD.document_dcn;
+			NEW.object_summary := 'This document''s DCN ' || NEW.document_dcn || ' is structurally invalid. '  || COALESCE(OLD.object_summary, '');
+		end if;
+	end if;
+	return NEW;
 end;
 $BODY$;
 
 ALTER FUNCTION public.tmt_dsobject_update()
     OWNER TO docushare_admin;
+
+CREATE TRIGGER tmt_dsobject_update
+    BEFORE UPDATE 
+    ON public.dsobject_table
+    FOR EACH ROW
+    WHEN ((old.handle_class = (2)::numeric))
+    EXECUTE PROCEDURE public.tmt_dsobject_update();
